fef0f1d02f19ca7b5bd4e5a0c3be26b7de544d10
rev5923 <55614611+rev5923@users.noreply.github.com>
2019-09-21 15:01:20 +0900

    Add files via upload

---
+++ js/jq.js
@@ -0,0 +1,19 @@
+$(function(){
+
+  // ヘッダータイトル 
+  $('.top-btn').click(function() {
+    $('html,body').animate({
+      'scrollTop': 0
+    }, 500);
+  });
+});
+
+  // ヘッダーメニュー
+$('header a').click(function() {
+  var id = $(this).attr('href');
+  var position = $(id).offset().top;
+  
+  $('html,body').animate({
+    'scrollTop': position
+  }, 500);
+});
